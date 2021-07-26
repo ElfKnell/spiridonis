751e49939e4f7fa832643c6972e5adb5bc72a124
ElfKnell <akyrych84@gmail.com>
2021-07-26 13:23:00 +0300

    feedback + name and message

--- feedback/views.py
+++ feedback/views.py
@@ -12,10 +12,11 @@ class ContactView(APIView):
         if serailizer_class.is_valid():
             data = serailizer_class.validated_data
             email_from = data.get('email')
-            #name = data.get('name')
+            name = data.get('name')
             subject = data.get('subject')
             message = data.get('message')
+            message_name = ' '.join([name, message])
             recipient_list = ('akyrych84@gmail.com', )
-            send_mail(subject, message, email_from, recipient_list)
+            send_mail(subject, message_name, email_from, recipient_list)
             return Response({"success": "Sent"}, status=status.HTTP_200_OK)
         return Response({"success": "Failed"}, status=status.HTTP_400_BAD_REQUEST)
